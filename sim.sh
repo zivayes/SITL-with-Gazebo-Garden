c6227c6fe2d0ed5678addf2c513c26df1006c8b7
zivayes <116758306+zivayes@users.noreply.github.com>
2023-03-29 12:24:22 +0300

    Add files via upload

---
+++ sim.sh
@@ -0,0 +1,5 @@
+export GAZEBO_MODEL_PATH=$HOME/ET-22-23-AUV-Simulation/plugins/freebuoyancy_gazebo/model:${GAZEBO_MODEL_PATH}
+export GAZEBO_RESOURCE_PATH=$HOME/ET-22-23-AUV-Simulation/plugins/freebuoyancy_gazebo/worlds:${GAZEBO_RESOURCE_PATH}
+export GAZEBO_MODEL_PATH=~/ardupilot_gazebo/models
+export GAZEBO_RESOURCE_PATH=~/ardupilot_gazebo/worlds:${GAZEBO_RESOURCE_PATH}
+
